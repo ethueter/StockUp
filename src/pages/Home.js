e1db8b97c9fd2a374f0b46340b1ffacf8213ba0f
Tyler Hueter <tylerhueter08@gmail.com>
2020-10-15 21:39:33 -0400

    wired up navbar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,25 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
+import { NavLink } from 'react-router-dom';
 
 const Home = () => {
-    
+    const [value, setValue] = useState(0);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
 
     return (
-       <Grid container spacing={3}>
+      <Grid container spacing={3}>
         <Grid item xs={12}>
-            <h1>Stock UP!</h1>
-            <h3>Build Your Fantasy Portfolio Today!</h3>
+          <h1>Stock UP!</h1>
+          <h3>Build Your Fantasy Portfolio Today!</h3>
         </Grid>
         <Paper className="navbar">
-            <Tabs>
-                <Tab></Tab>
-            </Tabs>
+          <Tabs
+            value={value}
+            onChange={handleChange}
+            indicatorColor="primary"
+            textColor="primary"
+            centered
+          >
+            <Tab 
+                label="Portfolio"
+                href="/portfolio"
+            >
+            </Tab>
+            <Tab
+                label="Trade"
+                href="/trade"
+            ></Tab>
+                    <Tab
+                        label="Research"
+                        href="/research"
+                    ></Tab>
+          </Tabs>
         </Paper>
-       </Grid>
-    )
+      </Grid>
+    );
 };
 
 export default Home;
