496271ed09abe1ac8d18b80e3d7e1887e9f15a59
Tyler Hueter <tylerhueter08@gmail.com>
2020-10-16 22:12:42 -0400

    home page design

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
 import Header from "../components/Header";
 import Navbar from "../components/Navbar";
 
@@ -8,9 +9,27 @@ const Home = () => {
     const tagline = "Build Your Fantasy Portfolio Today!";
 
     return (
-      <Grid container spacing={3}>
-        <Header tagline={tagline}/>
+      <Grid container spacing={2}>
+        <Header tagline={tagline} />
         <Navbar />
+        <Grid
+          container
+          direction="row"
+          justify="space-around"
+          alignItems="stretch"
+          spacing={3}
+        >
+          <Grid item lg="9">
+            <Paper>
+              <h2>Headline One</h2>
+            </Paper>
+          </Grid>
+          <Grid item lg="3">
+            <Paper>
+              <h2>About This Site</h2>
+            </Paper>
+          </Grid>
+        </Grid>
       </Grid>
     );
 };
