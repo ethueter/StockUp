d33db12589b2269e993aaf0b6d984e2db9db3788
Tyler Hueter <tylerhueter08@gmail.com>
2020-10-20 22:08:26 -0400

    beginning design for portfolio page

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -2,16 +2,41 @@ import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import Header from '../components/Header';
 import Navbar from '../components/Navbar';
+import { Card, Paper, Typography, CardContent } from '@material-ui/core';
 
 
 const Portfolio = () => {
     const tagline = "Your Portfolio"
     return (
-        <Grid container spacing={2}>
-            <Header tagline={tagline} />
-            <Navbar />
+      <Grid container spacing={2}>
+        <Header tagline={tagline} />
+        <Navbar />
+        <Grid container
+                direction="row"
+                justify="space-around"
+                alignItems="stretch"
+                spacing={3}>
+          <Grid item md="9">
+            <Paper>
+              <Typography variant="h4" gutterBottom>
+                Current Holdings
+              </Typography>
+            </Paper>
+          </Grid>
+          <Grid item md="3">
+            <Card variant="outlined">
+                <CardContent>
+                    <Typography variant="h4" gutterBottom>
+                        <Paper>
+                            <h4>Goals</h4>
+                        </Paper>
+                    </Typography>
+                </CardContent>
+            </Card>
+          </Grid>
         </Grid>
-    )
+      </Grid>
+    );
 }
 
 export default Portfolio;
