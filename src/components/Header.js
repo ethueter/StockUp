dc7c6aa0f0484e402f43d0b3a6a23ebd0a3bdc17
Tyler Hueter <tylerhueter08@gmail.com>
2020-10-28 20:26:26 -0400

    wiring up SignOut button

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,10 @@ import React from 'react';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 
+import SignOutButton from './SignOut';
 
-const Header = ({ tagline, login }) => {
+
+const Header = ({ tagline, login, loginTemp }) => {
     
 
     return (
@@ -15,10 +17,11 @@ const Header = ({ tagline, login }) => {
         <Grid  
             item
             xs={2}>
-            <Button onClick={login}>Account Login</Button>
+            <Button onClick={loginTemp}>Account Login</Button>
+          {login ? <SignOutButton /> : null}
         </Grid>
       </Grid>
     );
-}
+};
 
 export default Header;
