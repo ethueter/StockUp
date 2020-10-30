57e33c521638252972ef6900993da9cdfa24c812
Tyler Hueter <tylerhueter08@gmail.com>
2020-10-29 21:46:15 -0400

    reworded nav unstyled

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React, { useState, useEffect } from 'react';
-import { Router } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
 import "fontsource-roboto";
 
 import history from './services/history';
-import Routes from './routes';
+// import Routes from './routes';
 import { withFirebase } from './components/Firebase';
 // import Navbar from './components/Navbar';
 import Navigation from './components/Navigation';
 import Header from './components/Header';
+import Home from './pages/Home';
+import Portfolio from './pages/Portfolio';
+import Trade from './pages/Trade';
+import Research from './pages/Research';
+import SignUpPage from './components/SignUp';
 
 const App = ({ firebase }) => {
   const [authUser, setAuthUser] = useState(null);
@@ -27,7 +32,11 @@ const App = ({ firebase }) => {
       <Header />
       <Navigation />
       <hr />
-      <Routes authUser={authUser} />
+      <Route path="/" exact component={Home} />
+      <Route path="/portfolio" component={Portfolio} />
+      <Route path="/research" component={Research} />
+      <Route path="/trade" component={Trade} />
+      <Route path="/signup" component={SignUpPage} />
     </Router>
   );
 }
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -2,22 +2,25 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Navigation = () => (
-    <div>
-        <ul>
-            <li>
-                <Link to={"/"}>Home</Link>
-            </li>
-            <li>
-                <Link to={'/portfolio'}>Portfolio</Link>
-            </li>
-            <li>
-                <Link to={"/trade"}>Trade</Link>
-            </li>
-            <li>
-                <Link to={"/research"}>Research</Link>
-            </li>
-        </ul>
-    </div>
+  <div>
+    <ul>
+      <li>
+        <Link to={"/"}>Home</Link>
+      </li>
+      <li>
+        <Link to={"/portfolio"}>Portfolio</Link>
+      </li>
+      <li>
+        <Link to={"/trade"}>Trade</Link>
+      </li>
+      <li>
+        <Link to={"/research"}>Research</Link>
+      </li>
+      <li>
+        <Link to={"/signup"}>Sign Up</Link>
+      </li>
+    </ul>
+  </div>
 );
 
 export default Navigation;
