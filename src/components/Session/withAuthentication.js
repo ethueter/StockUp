0c0db442c1d7e385b8de6af703a48b13cdb8b4ec
Tyler Hueter <tylerhueter08@gmail.com>
2020-11-02 19:18:43 -0500

    movign auth logic from App to withAuthentication components

--- src/components/Session/withAuthentication.js
+++ src/components/Session/withAuthentication.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import AuthUserContact from './context';
+import { withFirebase } from '../Firebase';
 
 const withAuthentication = Component => {
-    class WithAuthentication extends React.Component {
-        render() {
-            return <Component {...this.props} />
-        }
+    const WithAuthentication = ({ firebase }) => {
+        const [authUser, setAuthUser] = useState(null);
+
+        useEffect(() => {
+            firebase.auth.onAuthStateChanged(authUser => {
+                authUser
+                    ? setAuthUser(authUser)
+                    : setAuthUser(null);
+            });
+        }, [firebase.auth])
+        
+            return (
+              <AuthUserContact.Provider value={authUser}>
+                <Component {...this.props} />
+              </AuthUserContact.Provider>
+            ); 
+        
     }
 
-    return WithAuthentication;
+    return withFirebase(WithAuthentication);
 };
 
 export default withAuthentication;
